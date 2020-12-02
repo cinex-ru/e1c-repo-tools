82d5748ded1963755e5f28989b7becb2a42f8057
cinex-ru <74475837+cinex-ru@users.noreply.github.com>
2020-12-02 09:51:29 +0700

    test(e1cdispatcher): add more readable describe texts

--- test/E1cDispatcher.spec.ts
+++ test/E1cDispatcher.spec.ts
@@ -3,69 +3,69 @@ import E1cDispatcher from '../src/E1cDispatcher';
 jest.mock('fs');
 const mockFs = jest.requireMock('fs');
 
-describe('Analyzing repo config', () => {
-    describe('config defined with all fields', () => {
-        it('if defined inits ok', async () => {
+describe('When init dispatcher with config', () => {
+    describe('and config defined with all fields', () => {
+        it('inits ok', async () => {
             expect(await E1cDispatcher.initWithLocalConfig()).toBeInstanceOf(E1cDispatcher);
         });
 
-        it('if defined get path to executable', async () => {
+        it('gets path to executable', async () => {
             expect((await E1cDispatcher.initWithLocalConfig()).pathToExecutable).toBe('path/to/executable');
         });
 
-        it('if defined get path to src dir', async () => {
+        it('gets path to src dir', async () => {
             expect((await E1cDispatcher.initWithLocalConfig()).pathToSrcDir).toBe('path/to/src');
         });
 
-        it('if defined get path to dist dir', async () => {
+        it('gets path to dist dir', async () => {
             expect((await E1cDispatcher.initWithLocalConfig()).pathToDistDir).toBe('path/to/dist');
         });
 
-        it('if defined get files extensions', async () => {
+        it('gets files extensions', async () => {
             expect((await E1cDispatcher.initWithLocalConfig()).filesExtensions).toEqual(['file', 'exts']);
         });
 
-        it('if defined get path to logs dir', async () => {
+        it('gets path to logs dir', async () => {
             expect((await E1cDispatcher.initWithLocalConfig()).pathToLogsDir).toBe('path/to/logs');
         });
     });
 
-    describe('config defined with path to executable only', () => {
+    describe('and config defined with path to executable only', () => {
         beforeAll(() => {
             mockFs.readFile.mockImplementation(() => Promise.resolve('{ "e1cRepoConfig": { "pathToExecutable": "some/path" } }'));
         });
 
-        it('if defined inits ok', async () => {
+        it('inits ok', async () => {
             expect(await E1cDispatcher.initWithLocalConfig()).toBeInstanceOf(E1cDispatcher);
         });
 
-        it('if defined get path to executable', async () => {
+        it('gets path to executable', async () => {
             expect((await E1cDispatcher.initWithLocalConfig()).pathToExecutable).toBe('some/path');
         });
 
-        it('if defined get default path to src dir', async () => {
+        it('gets default path to src dir', async () => {
             expect((await E1cDispatcher.initWithLocalConfig()).pathToSrcDir).toBe('./src');
         });
 
-        it('if defined get default path to dist dir', async () => {
+        it('gets default path to dist dir', async () => {
             expect((await E1cDispatcher.initWithLocalConfig()).pathToDistDir).toBe('./dist');
         });
 
-        it('if defined get default files extensions', async () => {
+        it('gets default files extensions', async () => {
             expect((await E1cDispatcher.initWithLocalConfig()).filesExtensions).toEqual(['erf', 'epf']);
         });
 
-        it('if defined get default path to logs dir', async () => {
+        it('gets default path to logs dir', async () => {
             expect((await E1cDispatcher.initWithLocalConfig()).pathToLogsDir).toBe('./logs');
         });
     });
 
-    describe('config defined with undefined path to executable', () => {
+    describe('and config defined without path to executable', () => {
         beforeAll(() => {
             mockFs.readFile.mockImplementation(() => Promise.resolve('{ "e1cRepoConfig": {} }'));
         });
 
-        it('if undefined path to executable throws an error', async () => {
+        it('throws an error', async () => {
             expect.assertions(1);
             try {
                 await E1cDispatcher.initWithLocalConfig();
@@ -78,12 +78,12 @@ describe('Analyzing repo config', () => {
         });
     });
 
-    describe('config not defined', () => {
+    describe('and config not defined', () => {
         beforeAll(() => {
             mockFs.readFile.mockImplementation(() => Promise.resolve('{}'));
         });
 
-        it('if not defined throws an error', async () => {
+        it('throws an error', async () => {
             expect.assertions(1);
             try {
                 await E1cDispatcher.initWithLocalConfig();
