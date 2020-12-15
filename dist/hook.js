24dee0a8d00ff842f03a7d49e29a2fd259995f5d
cinex-ru <74475837+cinex-ru@users.noreply.github.com>
2020-12-15 20:58:38 +0700

    build: remove dist files from git index

--- dist/hook.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const precommit_hook_1 = require("./precommit-hook");
-const console_operations_1 = require("./console-operations");
-console_operations_1.setLogUpdateStatus(false);
-precommit_hook_1.precommitHook();
